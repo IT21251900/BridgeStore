14132eb607ca0c26e496e089318e97fb18b86346
IT21302862 <sanayasamadhi@gmail.com>
2023-05-02 09:26:27 +0530

    ui added

---
+++ app/src/main/java/com/example/mad/DisplayProductCustomerActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DisplayProductCustomerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_product_customer)
+    }
+}
