3e8b866782903c8247f19ef4fac32a8ac2a518e9
IT21302862 <sanayasamadhi@gmail.com>
2023-05-13 18:41:51 +0530

    branch cleard

--- app/src/main/java/com/example/bridgestore/model/ChatMessage.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bridgestore.model
-
-data class ChatMessage(
-    var id:String?,
-    val senderId: String = "",
-    val recipientId: String = "",
-    val message: String = "",
-    val timestamp: Long = 0
-)
