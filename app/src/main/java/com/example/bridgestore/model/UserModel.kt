6762377efbb6bbc63bd1a3d1444ab02f420114ed
IT21302862 <sanayasamadhi@gmail.com>
2023-05-13 18:42:47 +0530

    Code Updated

---
+++ app/src/main/java/com/example/bridgestore/model/UserModel.kt
@@ -0,0 +1,12 @@
+package com.example.bridgestore.model
+
+data class UserModel(
+
+
+    val name: String = "",
+    val contactNumber: String = "",
+    val address: String = "",
+    val userRole: String = "",
+    val email: String = "",
+    val password: String = ""
+)
