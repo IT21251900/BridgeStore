db3bfc0ec12a3b99fad9347b7bbae78abf0b8908
IT21302862 <sanayasamadhi@gmail.com>
2023-05-13 18:44:30 +0530

    Main cleanup

--- app/src/main/java/com/example/bridgestore/model/Customer.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bridgestore.model
-
-data class Customer(
-    val id: String?,
-    val fullName: String,
-    val phone: String,
-    val email: String,
-    val password: String
-)
